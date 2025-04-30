b8a58f834e94522faf0d4985b6c36babd7aca032
Akash <aakashkumbhare@gmail.com>
2025-04-30 19:57:45 +0530

    Updadte

--- ReadMe.md
+++ ReadMe.md
@@ -8,37 +8,6 @@ Once installed, note the path (e.g., `C:\Program Files (x86)\gs\gs10.05.1\bin\gs
 
 ---
 
-### 🛠 Sample Batch File (`compress_pdfs.bat`):
-
-```bat
-@echo off
-setlocal
-
-:: Set the path to Ghostscript
-set "GS=C:\Program Files (x86)\gs\gs10.05.1\bin\gswin32c.exe"
-
-:: Directory containing PDFs
-set "SOURCE_FOLDER=D:\dev\compress-pdf"
-:: Directory to save compressed PDFs
-set "DEST_FOLDER=%SOURCE_FOLDER%\compressed"
-
-:: Create output directory if it doesn't exist
-if not exist "%DEST_FOLDER%" (
-    mkdir "%DEST_FOLDER%"
-)
-
-:: Loop through each PDF in the source folder
-for %%F in ("%SOURCE_FOLDER%\*.pdf") do (
-    echo Compressing: %%~nxF
-    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 ^
-        -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ^
-        -sOutputFile="%DEST_FOLDER%\%%~nxF" "%%F"
-)
-
-echo Done!
-pause
-```
-
 ---
 
 ### 📌 PDF Compression Levels (choose one for `-dPDFSETTINGS`):
--- compress_pdfs.bat
+++ compress_pdfs.bat
@@ -7,20 +7,24 @@ set "GS=C:\Program Files (x86)\gs\gs10.05.1\bin\gswin32c.exe"
 :: Directory containing PDFs
 set "SOURCE_FOLDER=D:\dev\compress-pdf"
 :: Directory to save compressed PDFs
-set "DEST_FOLDER=%SOURCE_FOLDER%\compressed"
+set "DEST_BASE=%SOURCE_FOLDER%\compressed"
 
-:: Create output directory if it doesn't exist
-if not exist "%DEST_FOLDER%" (
-    mkdir "%DEST_FOLDER%"
+:: Create subfolders for each compression level
+for %%S in (screen ebook printer prepress) do (
+    if not exist "%DEST_BASE%\%%S" (
+        mkdir "%DEST_BASE%\%%S"
+    )
 )
 
-:: Loop through each PDF in the source folder
+:: Process each PDF
 for %%F in ("%SOURCE_FOLDER%\*.pdf") do (
-    echo Compressing: %%~nxF
-    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 ^
-        -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ^
-        -sOutputFile="%DEST_FOLDER%\%%~nxF" "%%F"
+    echo Processing: %%~nxF
+
+    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%DEST_BASE%\screen\%%~nF_screen.pdf" "%%F"
+    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%DEST_BASE%\ebook\%%~nF_ebook.pdf" "%%F"
+    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%DEST_BASE%\printer\%%~nF_printer.pdf" "%%F"
+    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%DEST_BASE%\prepress\%%~nF_prepress.pdf" "%%F"
 )
 
-echo Done!
+echo Compression complete!
 pause
